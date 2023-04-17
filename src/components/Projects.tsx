4a4ac16bc414f17dd69df46997318d77c3d88cfe
mryevhenii07 <mrit.works07@gmail.com>
2023-04-17 23:20:59 +0300

    changed project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 // import CryptoImg from '../public/assets/project/crypto.jpg';
@@ -18,12 +17,12 @@ const Projects = () => {
         <p className="text-xl tracking-widest uppercase text-[#5651e5">Projects</p>
         <h2 className="py-4">What I Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem title="Wallet App" backgroundImg={WalletImg} projectUrl="/wallet" />
+          <ProjectItem title="Git Online" backgroundImg={GitonlineImg} projectUrl="/gitonline" />
+          <ProjectItem title="Search Event" backgroundImg={SearchEvenImg} projectUrl="/event" />
           <ProjectItem title="Crypto App" backgroundImg={CryptoImg} projectUrl="/crypto" />
           <ProjectItem title="Pizza King" backgroundImg={PizzaImg} projectUrl="/pizza" />
-          <ProjectItem title="Search Event" backgroundImg={SearchEvenImg} projectUrl="/event" />
-          <ProjectItem title="Git Online" backgroundImg={GitonlineImg} projectUrl="/gitonline" />
           <ProjectItem title="Ice Cream" backgroundImg={IceImg} projectUrl="/icecream" />
-          <ProjectItem title="Wallet App" backgroundImg={WalletImg} projectUrl="/wallet" />
         </div>
       </div>
     </div>
